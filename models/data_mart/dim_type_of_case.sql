9f8adb491ba44c598e42279062b596b892fa2086
Eric Ejimba <ejimba@gmail.com>
2024-09-06 09:38:06 +0300

    replace nulls with unknown for type of case

--- models/data_mart/dim_type_of_case.sql
+++ models/data_mart/dim_type_of_case.sql
@@ -1,14 +1,15 @@
 {{ config(
     materialized = 'table',
     indexes=[
-      {'columns': ['name']},
+      {'columns': ['type_of_case']},
     ]
 )}}
 
 SELECT
-    DISTINCT type_of_case AS name,
+    DISTINCT type_of_case AS type_of_case,
     current_date AS load_date
 FROM
     {{ ref('fct_case_linelist') }} as linelist
+WHERE type_of_case IS NOT NULL
 ORDER BY
     type_of_case
